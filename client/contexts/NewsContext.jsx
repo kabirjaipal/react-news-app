c992edb53a072fe2149558e3965b5e7154cc2bc1
kabirsingh2004 <kabirjaipal447@gmail.com>
2024-02-21 21:23:20 +0530

    fixed bugs ✅

--- client/contexts/NewsContext.jsx
+++ client/contexts/NewsContext.jsx
@@ -3,9 +3,3 @@ import { createContext } from "react";
 export const NewsContext = createContext();
 
 export const NewsContextProvider = NewsContext.Provider;
-
-import { createContext } from "react";
-
-export const NewsContext = createContext();
-
-export const NewsContextProvider = NewsContext.Provider;
